0d695fab58839b2c6c51a373beac6ac7c2dbf6de
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-01 12:57:12 +0100

    update config with auth0 variables

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -19,6 +19,12 @@ def __get_env_var_as_boolean(name: str) -> bool | None:
 
 
 app_config = SimpleNamespace(
+    api_key=__get_env_var("API_KEY"),
+    auth0=SimpleNamespace(
+        domain=__get_env_var("AUTH0_DOMAIN"),
+        client_id=__get_env_var("AUTH0_CLIENT_ID"),
+        client_secret=__get_env_var("AUTH0_CLIENT_SECRET"),
+    ),
     logging_level=__get_env_var("LOGGING_LEVEL"),
     postgres=SimpleNamespace(
         user=__get_env_var("POSTGRES_USER"),
@@ -27,5 +33,7 @@ app_config = SimpleNamespace(
         host=__get_env_var("POSTGRES_HOST"),
         port=__get_env_var("POSTGRES_PORT"),
     ),
-    api_key=__get_env_var("API_KEY"),
+    sentry=SimpleNamespace(
+        dsn_key=__get_env_var("SENTRY_DSN_KEY"), environment=__get_env_var("SENTRY_ENV")
+    ),
 )
