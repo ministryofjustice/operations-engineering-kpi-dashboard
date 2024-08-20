5c350c35af068801b33437322d1a77ac0ff38aa1
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-08-20 09:47:51 +0100

    Amend conflicts with current file
    figure_service.py had conflicts with main, this resolves that issue.
    Also updated support stats code to bring it in line with refactoring in main

--- app/services/figure_service.py
+++ app/services/figure_service.py
@@ -18,14 +18,17 @@ class FigureService:
             columns=["timestamp", "count"],
         ).sort_values(by="timestamp", ascending=True)
 
-        return px.line(
+        fig_number_of_repositories_with_standards_label = px.line(
             number_of_repos_with_standards_label_df,
             x="timestamp",
             y="count",
             title="🏷️ Number of Repositories With Standards Label",
             markers=True,
             template="plotly_dark",
-        ).add_hline(y=0)
+        )
+        fig_number_of_repositories_with_standards_label.add_hline(y=0)
+
+        return fig_number_of_repositories_with_standards_label
 
     def get_stubbed_number_of_repositories_with_standards_label_dashboard(self):
         number_of_repos_with_standards_label_df = pd.DataFrame(
@@ -35,14 +38,17 @@ class FigureService:
             columns=["timestamp", "count"],
         ).sort_values(by="timestamp", ascending=True)
 
-        return px.line(
+        fig_stubbed_number_of_repositories_with_standards_label = px.line(
             number_of_repos_with_standards_label_df,
             x="timestamp",
             y="count",
             title="🏷️ Number of Repositories With Standards Label",
             markers=True,
             template="plotly_dark",
-        ).add_hline(y=0)
+        )
+        fig_stubbed_number_of_repositories_with_standards_label.add_hline(y=0)
+
+        return fig_stubbed_number_of_repositories_with_standards_label
 
     def get_stubbed_number_of_repositories_archived_by_automation(self):
         number_of_repositories_archived_by_automation = pd.DataFrame(
@@ -52,14 +58,17 @@ class FigureService:
             columns=["timestamp", "count"],
         ).sort_values(by="timestamp", ascending=True)
 
-        return px.line(
+        fig_stubbed_number_of_repositories_archived_by_automation = px.line(
             number_of_repositories_archived_by_automation,
             x="timestamp",
             y="count",
             title="👴 Number of Repositories Archived By Automation",
             markers=True,
             template="plotly_dark",
-        ).add_hline(y=0)
+        )
+        fig_stubbed_number_of_repositories_archived_by_automation.add_hline(y=0)
+
+        return fig_stubbed_number_of_repositories_archived_by_automation
 
     def get_stubbed_sentry_transactions_used(self):
         sentry_transaction_quota_consumed = pd.DataFrame(
@@ -69,25 +78,26 @@ class FigureService:
             columns=["timestamp", "count"],
         ).sort_values(by="timestamp", ascending=True)
 
-        return (
-            px.line(
-                sentry_transaction_quota_consumed,
-                x="timestamp",
-                y="count",
-                title="👀 Sentry Transactions Used",
-                markers=True,
-                template="plotly_dark",
-            )
-            .add_hline(y=967741, annotation_text="Max Daily Usage")
-            .add_hrect(
-                y0=(967741 * 0.8),
-                y1=967741,
-                line_width=0,
-                fillcolor="red",
-                opacity=0.2,
-                annotation_text="Alert Threshold",
-            )
+        fig_stubbed_sentry_transactions_used = px.line(
+            sentry_transaction_quota_consumed,
+            x="timestamp",
+            y="count",
+            title="👀 Sentry Transactions Used",
+            markers=True,
+            template="plotly_dark",
         )
+        fig_stubbed_sentry_transactions_used.add_hline(
+            y=967741, annotation_text="Max Daily Usage"
+        )
+        fig_stubbed_sentry_transactions_used.add_hrect(
+            y0=(967741 * 0.8),
+            y1=967741,
+            line_width=0,
+            fillcolor="red",
+            opacity=0.2,
+            annotation_text="Alert Threshold",
+        )
+        return fig_stubbed_sentry_transactions_used
 
     def get_support_stats(self):
         support_stats_csv = pd.read_csv("data/support-stats.csv")
@@ -113,7 +123,7 @@ class FigureService:
             ignore_index=True,
         )
 
-        return px.line(
+        fig_support_stats = px.line(
             support_stats_csv_pivoted,
             x="Month",
             y="Count",
@@ -123,7 +133,9 @@ class FigureService:
             template="plotly_dark",
         )
 
-    def get_support_stats_all(self):
+        return fig_support_stats
+
+    def get_support_stats_year_to_date(self):
         support_requests_all = pd.read_csv("data/testing_support_stats.csv")
         support_requests_all = (
             support_requests_all.groupby(by=["Date", "Type"])
@@ -131,7 +143,7 @@ class FigureService:
             .reset_index(name="Count")
         )
 
-        return px.line(
+        fig_support_stats_year_to_date = px.line(
             support_requests_all,
             x="Date",
             y="Count",
@@ -139,6 +151,8 @@ class FigureService:
             title="Support Requests by Type - Year to Date",
             template="plotly_dark",
         )
+
+        return fig_support_stats_year_to_date
 
     def get_support_stats_current_month(self):
         month = date.today().month
@@ -153,7 +167,7 @@ class FigureService:
             support_requests_current_month.groupby("Date")["Type"].transform("size")
         )
 
-        return px.bar(
+        fig_support_stats_current_month = px.bar(
             support_requests_current_month,
             x="Date",
             y="Type",
@@ -161,6 +175,8 @@ class FigureService:
             title="Support Requests by Type - Current Month",
             template="plotly_dark",
         )
+
+        return fig_support_stats_current_month
 
     def get_github_actions_quota_usage(self):
         github_usage_csv = pd.read_csv(
@@ -172,23 +188,25 @@ class FigureService:
         )
         github_actions_summed["Date"] = pd.to_datetime(github_actions_summed["Date"])
 
-        return (
-            px.scatter(
-                github_actions_summed,
-                x="Date",
-                y="Quantity",
-                title="💥 GitHub Quota Usage",
-                trendline="ols",
-                template="plotly_dark",
-                hover_data=["Price Per Unit ($)"],
-            )
-            .add_hline(y=(40000 / 31), annotation_text="Max Daily Actions Usage Usage")
-            .add_hrect(
-                y0=((40000 / 31) * 0.8),
-                y1=(40000 / 31),
-                line_width=0,
-                fillcolor="red",
-                opacity=0.2,
-                annotation_text="Actions Alert Threshold",
-            )
+        fig_github_actions_quota_usage = px.scatter(
+            github_actions_summed,
+            x="Date",
+            y="Quantity",
+            title="💥 GitHub Quota Usage",
+            trendline="ols",
+            template="plotly_dark",
+            hover_data=["Price Per Unit ($)"],
         )
+        fig_github_actions_quota_usage.add_hline(
+            y=(40000 / 31), annotation_text="Max Daily Actions Usage Usage"
+        )
+        fig_github_actions_quota_usage.add_hrect(
+            y0=((40000 / 31) * 0.8),
+            y1=(40000 / 31),
+            line_width=0,
+            fillcolor="red",
+            opacity=0.2,
+            annotation_text="Actions Alert Threshold",
+        )
+
+        return fig_github_actions_quota_usage
