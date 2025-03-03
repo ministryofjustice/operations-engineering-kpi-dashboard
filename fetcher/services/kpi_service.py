dc30a217aac7edc126dd624b6d696bd386f3c75b
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2025-03-03 16:14:07 +0000

    updated kpi service

--- fetcher/services/kpi_service.py
+++ fetcher/services/kpi_service.py
@@ -32,3 +32,7 @@ class KpiService:
 
     def track_github_usage_report_daily(self, report_date: datetime.date, report_usage_data: dict):
         self.__post("/api/github_usage_report/add", {"report_date": report_date, "report_usage_data": report_usage_data})
+
+    def post_github_repository_metadata(self, github_id: int, name: str, full_name: str, owner: str, visibility: str):
+        self.__post("/api/github_repository_metadata/add",
+                    {"github_id": github_id, "name": name,"full_name": full_name, "owner": owner, "visibility": visibility})
