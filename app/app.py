f6822c16aab585c86d22ae06a31605417fe4e557
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2025-07-30 10:01:55 +0100

    Removed add_public_routes

--- app/app.py
+++ app/app.py
@@ -35,14 +35,7 @@ def create_app() -> Flask:
             force_https_callback=True,
             secure_session=True,
         )
-        add_public_routes(
-            app,
-            routes=[
-                "/api/indicator/add",
-                "/api/github_usage_report/add",
-                "/api/github_repository_metadata/add",
-            ],
-        )
+
         auth.register_provider(
             "idp",
             token_endpoint_auth_method="client_secret_post",
